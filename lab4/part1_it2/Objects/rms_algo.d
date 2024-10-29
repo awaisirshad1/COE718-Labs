50b98052d35c85658c17ec6f89fc30b5dfa85e85
Awais Irshad <a2irshad@opera.ee.ryerson.ca>
2024-10-29 11:04:19 -0400

    continuing lab4

---
+++ lab4/part1_it2/Objects/rms_algo.d
@@ -0,0 +1,15 @@
+.\objects\rms_algo.o: rms_algo.c
+.\objects\rms_algo.o: osObjects.h
+.\objects\rms_algo.o: C:\Users\ECBME\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\RTOS\RTX\INC\cmsis_os.h
+.\objects\rms_algo.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\rms_algo.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+.\objects\rms_algo.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\rms_algo.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
+.\objects\rms_algo.o: C:\Users\ECBME\AppData\Local\Arm\Packs\Keil\MDK-Middleware\7.12.0\Board\Board_LED.h
+.\objects\rms_algo.o: C:\Users\ECBME\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.6.0\Device\Include\LPC17xx.h
+.\objects\rms_algo.o: C:\Users\ECBME\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\core_cm3.h
+.\objects\rms_algo.o: C:\Users\ECBME\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\rms_algo.o: C:\Users\ECBME\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\rms_algo.o: C:\Users\ECBME\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\rms_algo.o: C:\Users\ECBME\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\mpu_armv7.h
+.\objects\rms_algo.o: C:\Users\ECBME\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.6.0\Device\Include\system_LPC17xx.h
